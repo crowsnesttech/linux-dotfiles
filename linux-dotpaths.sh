d1772fdd536992bbcc302031a48607fb020525d1
CrowsNestTech <crowsnesttech@gmail.com>
2020-12-05 15:17:34 -0700

    modified script

--- linux-dotpaths.sh
+++ linux-dotpaths.sh
@@ -9,3 +9,5 @@ sleep 2
 ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.oh-my-zsh ~/
 ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.vimrc ~/
 ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.zshrc ~/
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
