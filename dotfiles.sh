7290307116e77da44a4bdd8eb6a422a74a9b6a80
CrowsNestTech <crowsnesttech@gmail.com>
2020-12-05 14:22:25 -0700

    added custom folder

---
+++ dotfiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Clone Linux-Dotfiles from Github
+git clone https://github.com/crowsnesttech/linux-dotfiles.git 
+
+# if [ $? != "0" ]
+#   then
+#    echo "Failed"
+#  else
+#    echo "Success!"
+# fi
